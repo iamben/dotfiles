35d71f8f980621f29d231958fb0dadb41f23efa9
Lung-Pin Chang <changlp@cs.nctu.edu.tw>
2010-10-20 14:16:40 +0800

    As prev. commit.

--- .zsh/os/SunOS.zsh
+++ .zsh/os/SunOS.zsh
@@ -2,7 +2,6 @@
 LC_ALL=zh_TW.UTF-8
 LANG=zh_TW.UTF-8
 TERM=xtermc
-PATH=/usr/ucb:${PATH}:/opt/csw/bin:/usr/pkg/bin:/usr/pkg/sbin:/usr/ucb:/opt/csw/gcc3/bin/
 MANPATH=/usr/share/man:/usr/local/share/man:/opt/csw/share/man
 which ggrep > /dev/null && alias grep='ggrep --color=always'
 which glocate > /dev/null && alias locate=glocate
