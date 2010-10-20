685263fb7509c030d8eb5ebf524e646b5a00a298
Lung-Pin Chang <changlp@cs.nctu.edu.tw>
2010-10-20 14:20:44 +0800

    Add ls alias in SunOS.

--- .zsh/os/SunOS.zsh
+++ .zsh/os/SunOS.zsh
@@ -3,5 +3,6 @@ LC_ALL=zh_TW.UTF-8
 LANG=zh_TW.UTF-8
 TERM=xtermc
 MANPATH=/usr/share/man:/usr/local/share/man:/opt/csw/share/man
-which ggrep > /dev/null && alias grep='ggrep --color=always'
+which ggrep > /dev/null && alias g='ggrep --color=always'
 which glocate > /dev/null && alias locate=glocate
+alias ls='/usr/local/bin/ls --color=auto'
